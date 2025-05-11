c7b32c2b1df2a9a564c2cd2777d1de41742277fd
ayaarragab <ayarsk4343@gmail.com>
2025-05-12 01:13:13 +0300

    fix score coins

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,10 +4,12 @@ public class Coin : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("OnTriggerEnter called with: " + other.name);
         PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
 
         if (playerInventory != null)
         {
+            Debug.Log("not null");
             playerInventory.CoinsCollected();
             gameObject.SetActive(false);
         }
